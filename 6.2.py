23c9ead7dcecf8505911370a0dbf233f809db3ec
SoloVoice <a.v.rustamov@gmail.com>
2020-10-26 13:52:04 +0300

    2 задание исправлено

--- 6.2.py
+++ 6.2.py
@@ -2,10 +2,13 @@ class Road:
     _length = 0
     _width = 0
 
-    def __init__(self):
+    def calculation(self):
         self._length = int(input("Введите длинну полотна: "))
         self._width = int(input("Введите ширину полотна: "))
-        print(f"Необходимая масса асфальта: {self._length*self._width*25*5}")
+        print(f"Необходимая масса асфальта: {self._length * self._width * 25 * 5}")
+
+    def __init__(self):
+        self.calculation()
 
 
 a = Road()
